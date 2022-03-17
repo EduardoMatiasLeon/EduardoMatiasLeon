1a7efdc8b6ef95c78e207b0c5ed61537c2faafe0
EduardoMatiasLeon <97908617+EduardoMatiasLeon@users.noreply.github.com>
2022-03-17 16:12:53 -0600

    Add files via upload

---
+++ crowdstrike.ps1
@@ -0,0 +1,2 @@
+﻿net use \\10.76.11.214 /user:Administrator S0p0rt3
+\\10.76.11.214\Software\Crowdstrike.exe /install /quiet /norestart CID=651A9472616E44A68B8F756281B9738C-98 GROUPING_TAGS="Tecmilenio,1101-LT"
